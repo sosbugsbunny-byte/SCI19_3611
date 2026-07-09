738b672eb5c00a0d998de543c831ede4b30d14e8
peanutbutterjourney <raritaromlumduan@gmail.com>
2026-07-09 17:41:57 +0700

    first commit

---
+++ tutorial_code/python_basics/helloWorld.py
@@ -0,0 +1,6 @@
+print("Rarita  test.py:1 - helloWorld.py:1")
+print("B6722241  test.py:2 - helloWorld.py:2")
+print("I am learing python  test.py:3 - helloWorld.py:3")
+
+course = "SCI11693"
+print(f"Course: {course}  test.py:4 - helloWorld.py:6")
