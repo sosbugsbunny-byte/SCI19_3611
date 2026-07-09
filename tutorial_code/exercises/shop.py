5e52fb4f2a6c413f1ad032f02daf522322feff66
peanutbutterjourney <raritaromlumduan@gmail.com>
2026-07-09 18:38:56 +0700

    Update python exercises

--- tutorial_code/exercises/shop.py
+++ tutorial_code/exercises/shop.py
@@ -1,6 +1,15 @@
-ส# shop.py
+# shop.py
 # -------
-
+# Licensing Information:  You are free to use or extend these projects for
+# educational purposes provided that (1) you do not distribute or publish
+# solutions, (2) you retain this notice, and (3) you provide clear
+# attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
+# 
+# Attribution Information: The Pacman AI projects were developed at UC Berkeley.
+# The core projects and autograders were primarily created by John DeNero
+# (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
+# Student side autograding was added by Brad Miller, Nick Hay, and
+# Pieter Abbeel (pabbeel@cs.berkeley.edu).
 
 
 class FruitShop:
@@ -15,7 +24,7 @@ class FruitShop:
         """
         self.fruitPrices = fruitPrices
         self.name = name
-        print('Welcome to %s fruit shop - shop.py:18' % (name))
+        print('Welcome to %s fruit shop' % (name))
 
     def getCostPerPound(self, fruit):
         """
