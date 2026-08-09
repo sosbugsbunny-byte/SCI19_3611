037150e748977b0da5191ca109ff8276ccc33a74
B6722241 <raritaromlumduan@gmail.com>
2026-08-09 20:33:11 +0700

    ลบไฟลและฟลเดอรอื่นๆ ออก เกบไว้แค่ SCI193611

--- tutorial_code/exercises/tools/projectParams.py
+++
@@ -1,9 +0,0 @@
-# projectParams.py
-# ----------------
-
-
-
-STUDENT_CODE_DEFAULT = 'addition.py,buyLotsOfFruit.py,shopSmart.py,tools/shopAroundTown.py'
-PROJECT_TEST_CLASSES = 'tools/tutorialTestClasses.py'
-PROJECT_NAME = 'Project 0: Tutorial'
-BONUS_PIC = False
